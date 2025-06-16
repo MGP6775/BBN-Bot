41269c1793e0df9ab8debb9e19653c7434a905fa
Angry007-Julian <julian.heenes@gmail.com>
2025-06-16 15:20:55 +0200

    change: outsource roles and channel ids

---
+++ const.ts
@@ -0,0 +1,14 @@
+// Roles
+
+export const supportRole = "1120392307087261787" // Support role
+export const ownerRoleID = "757969277063266407" // Owner role ID
+
+export const supportRoles = [ ownerRoleID, "815298088184446987", supportRole ] // Owner, Dev, Support
+
+export const verified = "757983851032215673" // Verified role
+
+// Channels
+
+export const createTicketChannelID = "1081337337704886392" // Channel ID for creating tickets
+export const firstLevelSupportCategoryID = "1081347349462405221" // Category ID for first level support
+export const secondLevelSupportCategoryID = "1120395441138315345" // Category ID for second level support
